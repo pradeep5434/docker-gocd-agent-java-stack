a8a19282f589925de27162a97712f21855519012
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-02-06 20:49:54 +0530

    Fixing issues with package installation

--- packages/run.sh
+++ packages/run.sh
@@ -5,12 +5,14 @@ set -ex
 ## Install the required packages
 export PACKAGES_ROOT_DIR=$(cd `dirname $0`/; pwd -P)
 
+echo "Attempting to read all the packages under ${PACKAGES_ROOT_DIR}"
+
 ## Read through all the folders and attempt installing them
-while read package; do
-	echo "Checking for an install script for ${package}"
-	if [ -e "${package}/install.sh" ]; then
-		echo "Attempting to install package - ${package}"
-		bash ${package}/install.sh
-		echo "${package} installation completed"
-	fi
-done < `ls ${PACKAGES_ROOT_DIR}`
+ls ${PACKAGES_ROOT_DIR}/ | while read package; do
+  echo "Checking for an install script for ${package}"
+  if [ -e "${package}/install.sh" ]; then
+    echo "Attempting to install package - ${package}"
+    bash ${package}/install.sh
+    echo "${package} installation completed"
+  fi
+done
