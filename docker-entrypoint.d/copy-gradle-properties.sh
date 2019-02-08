20ee1196f5ec7bf79e29c491f575b3da7ce4c890
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-02-08 08:00:39 +0530

    Adding logs and changing ownership of the gradle.properties inside the /home/go

--- docker-entrypoint.d/copy-gradle-properties.sh
+++ docker-entrypoint.d/copy-gradle-properties.sh
@@ -2,6 +2,9 @@
 
 ## Copy the secret (gradle.properties) to it's location
 if [ -n ${GRADLE_SECRETS_MOUNT_DIR} ]; then
+  echo "Copying ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties to /home/go/.gradle"
   # if this environment variable is found, copy the gradle.properties from this location to /home/go/.gradle/gradle.properties
   cp ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties /home/go/.gradle/gradle.properties
+  chown -R go:go /home/go/.gradle/
+  echo "Copy complete for ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties to /home/go/.gradle"
 fi
