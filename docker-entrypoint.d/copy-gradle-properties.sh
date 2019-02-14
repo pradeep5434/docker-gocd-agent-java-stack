2d0458cb717408564bafff6ecf79a7fbf26fbb60
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-02-14 22:18:15 +0530

    Change the ownership only for gradle.properties

--- docker-entrypoint.d/copy-gradle-properties.sh
+++ docker-entrypoint.d/copy-gradle-properties.sh
@@ -5,6 +5,6 @@ if [ -n ${GRADLE_SECRETS_MOUNT_DIR} ]; then
   echo "Copying ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties to /home/go/.gradle"
   # if this environment variable is found, copy the gradle.properties from this location to /home/go/.gradle/gradle.properties
   cp ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties /home/go/.gradle/gradle.properties
-  chown -R go:go /home/go/.gradle/
+  chown go:go /home/go/.gradle/gradle.properties
   echo "Copy complete for ${GRADLE_SECRETS_MOUNT_DIR}/gradle.properties to /home/go/.gradle"
 fi
